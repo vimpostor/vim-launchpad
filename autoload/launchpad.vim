vimpostor/vim-launchpad

commit f74d490889c032946002f45a0c78ea7c55e2feec

Add more options to close the preview window

autoload/launchpad.vim
@@ -1,6 +1,7 @@
 let s:job_lines = []
 let s:launch_buf = -1
 let s:launch_running = 0
+let s:job_killed = 0
 
 func launchpad#init()
 	let g:launchpad_options = extend(launchpad#default_options(), get(g:, 'launchpad_options', {}))
@@ -18,6 +19,7 @@ func launchpad#default_options()
 		\ autojump: 1,
 		\ autoopenquickfix: 1,
 		\ autosave: 1,
+		\ closepreview: "auto",
 		\ default_mappings: 1,
 	\ }
 endfunc
@@ -53,6 +55,7 @@ func launchpad#launch()
 		exe 'bdelete ' . s:launch_buf
 		let s:launch_buf = -1
 	endif
+	let s:job_killed = 0
 	call launchpad#lib#launch()
 	let s:launch_running = 1
 endfunc
@@ -61,6 +64,7 @@ func launchpad#stop()
 	if !s:launch_running
 		return
 	endif
+	let s:job_killed = 1
 	if has('nvim')
 		call jobstop(s:job)
 	else
@@ -91,9 +95,18 @@ func launchpad#build_cb(j, s)
 	endif
 endfunc
 
+func launchpad#close_preview()
+	if g:launchpad_options.closepreview == "never"
+		return
+	endif
+	if s:job_killed || !a:s || g:launchpad_options.closepreview == "always"
+		pclose
+	endif
+endfunc
+
 func launchpad#launch_cb(j, s)
 	let s:launch_running = 0
-	pclose
+	call launchpad#close_preview()
 	echom 'Program quit with exit code ' . a:s
 endfunc
 

commit f0e1dadd81349c1c4be58a9293751a28f8a472e7

Fix preview not closing
Regression was introduced in f74d490889c032946002f45a0c78ea7c55e2feec.
The status needs to be passed along.

autoload/launchpad.vim
@@ -97,7 +97,7 @@ func launchpad#build_cb(j, s)
 	endif
 endfunc
 
-func launchpad#close_preview()
+func launchpad#close_preview(s)
 	if g:launchpad_options.closepreview == "never"
 		return
 	endif
@@ -108,7 +108,7 @@ endfunc
 
 func launchpad#launch_cb(j, s)
 	let s:launch_running = 0
-	call launchpad#close_preview()
+	call launchpad#close_preview(a:s)
 	echom 'Program quit with exit code ' . a:s
 endfunc
 